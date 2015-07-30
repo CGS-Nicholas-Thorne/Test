91e3202a55125f24bdd42be5dd9015b7243eb91b
Nicholas Thorne <1645@cgs.act.edu.au>
2015-07-30 15:19:34 +1000

    Second view controller

---
+++ TestProject/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TestProject
+//
+//  Created by Nicholas Thorne on 30/07/2015.
+//  Copyright (c) 2015 Nicholas Thorne. All rights reserved.
+//
+
+import Foundation
---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by Nicholas Thorne on 30/07/2015.
+//  Copyright (c) 2015 Nicholas Thorne. All rights reserved.
+//
+
+import Foundation

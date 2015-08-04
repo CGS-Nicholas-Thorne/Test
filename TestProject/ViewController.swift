f09069f15520d9657ff969c712a9c34e3faafd0b
Nicholas Thorne <1645@cgs.act.edu.au>
2015-08-04 12:23:17 +1000

    feature-k

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func awesomeFeatureX() {
+    func awesomeFeatureG() {
         
         println("Jebus")
         
